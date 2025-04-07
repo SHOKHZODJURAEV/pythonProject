17fdfd45729a6dad1ccf1b2dc6af0ef6bccbe934
SHOKHZODJURAEV <85317197+SHOKHZODJURAEV@users.noreply.github.com>
2025-04-07 16:28:03 -0700

    python start with Muhtar

---
+++ python_cydeo/day01/first_python_program.py
@@ -0,0 +1,12 @@
+print('Python Programming Language')
+# This is a comment
+
+"""
+Multi
+line
+comments
+"""
+
+print("I lobe \"Python\"")
+print('I love "Python"')
+
---
+++ python_cydeo/day01/variables.py
@@ -0,0 +1,50 @@
+"""
+Java Variables or in static typing programming languages:
+
+    String name = "John Doe";
+
+Python Variables or in dynamic typed programming languages:
+    name = "John Doe"
+
+
+"""
+# String
+name_of_student = "John Doe"
+print(name_of_student)
+print(type(name_of_student))
+
+#float
+score = 99.9
+print(type(score))
+print(score)
+
+#int
+age = 20
+print(age)
+print(type(age))
+
+#boolean
+is_student = True
+print(is_student)
+print(type(is_student))
+
+s = '35'
+
+conNum = int(s)
+print(conNum)
+print(type(conNum))
+
+age = 20
+print("My age is " + str(age))
+print("My age is {}".format(age))
+print(f"My age is {age}")
+name = "John Doe"
+print("My name is " + name)
+print("My name is {} and my age is {}".format(name, age))
+print(f"My name is {name} and my age is {age}")
+
+
+
+
+
+
